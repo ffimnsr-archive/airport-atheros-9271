bedf5b96e62d80e2481128ff7d35f1790cabeea5
ffimnsr <ffimnsr@gmail.com>
2019-07-07 05:07:54 +0800

    update instructions

--- AirPortAtheros9271/AirPortAtheros9271.cpp
+++ AirPortAtheros9271/AirPortAtheros9271.cpp
@@ -8,6 +8,7 @@
 #include <IOKit/IOMessage.h>
 #include <IOKit/usb/IOUSBHostDevice.h>
 #include <IOKit/usb/IOUSBHostInterface.h>
+#include <IOKit/network/IOGatedOutputQueue.h>
 #pragma clang diagnostic pop
 
 #include "AirPortAtheros9271.hpp"
@@ -33,6 +34,10 @@ IOService *AirPort_Atheros9271::probe(IOService *provider, SInt32 *score)
     IOLog("Probing AirPortAtheros9271 driver.\n");
     IOLog("Version %s starting on OS X Darwin %d.%d.\n", OSKextGetCurrentVersionString(), version_major, version_minor);
     
+    fCompletionLock = IOLockAlloc();
+    if (!fCompletionLock)
+        return NULL;
+    
     setDevice(provider);
     if (fDevice == NULL)
     {
@@ -45,8 +50,8 @@ IOService *AirPort_Atheros9271::probe(IOService *provider, SInt32 *score)
     fDeviceRev = USBToHost16(fDevice->getDeviceDescriptor()->bcdDevice);
     
     pushDeviceFirmware();
-    
-    return NULL;
+
+    return super::probe(provider, score);
 }
 
 bool AirPort_Atheros9271::attach(IOService *provider)
@@ -71,6 +76,23 @@ bool AirPort_Atheros9271::start(IOService *provider)
     snprintf(buf, sizeof(buf), "%s %s", OSKextGetCurrentIdentifier(), OSKextGetCurrentVersionString());
     setProperty("Atheros Driver Version", buf);
     
+    
+    if (!createMediumTables())
+    {
+        IOLog("Unable to create medium tables.\n");
+        return false;
+    }
+    
+    //    if (!createNetworkInterface())
+    //    {
+    //        IOLog("Unable to create network interface.\n");
+    //        return NULL;
+    //    }
+    //
+    
+    setLinkStatus(kIONetworkLinkValid);
+    
+    
     PMinit();
     provider->joinPMtree(this);
     
@@ -81,6 +103,15 @@ void AirPort_Atheros9271::stop(IOService *provider)
 {
     IOLog("Stopping AirPortAtheros9271 driver.\n");
     PMstop();
+    
+    if (fCompletionLock)
+    {
+        IOLockFree(fCompletionLock);
+        fCompletionLock = NULL;
+    }
+    
+    OSSafeReleaseNULL(fNetworkInterface);
+    
     setDevice(NULL);
     super::stop(provider);
 }
@@ -103,6 +134,10 @@ IOReturn AirPort_Atheros9271::message(UInt32 type, IOService *provider, void *ar
 
 bool AirPort_Atheros9271::willTerminate(IOService *provider, IOOptionBits options)
 {
+//    getOutputQueue()->stop();
+//    getOutputQueue()->flush();
+//    getOutputQueue()->setCapacity(0);
+    
     return super::willTerminate(provider, options);
 }
 
@@ -118,6 +153,50 @@ bool AirPort_Atheros9271::finalize(IOOptionBits options)
 
 IOReturn AirPort_Atheros9271::getHardwareAddress(IOEthernetAddress *addr)
 {
+    addr->bytes[0] = 0x28;
+    addr->bytes[1] = 0xe3;
+    addr->bytes[2] = 0x47;
+    addr->bytes[3] = 0x15;
+    addr->bytes[4] = 0x69;
+    addr->bytes[5] = 0x52;
+    return kIOReturnSuccess;
+}
+
+IOReturn AirPort_Atheros9271::getPacketFilters(const OSSymbol *group, UInt32 *filters) const
+{
+    IOReturn result = kIOReturnSuccess;
+    
+    if (group == gIOEthernetWakeOnLANFilterGroup)
+        *filters = 0;
+    else if (group == gIONetworkFilterGroup)
+    {
+        *filters = kIOPacketFilterUnicast | kIOPacketFilterBroadcast | kIOPacketFilterPromiscuous | kIOPacketFilterMulticast | kIOPacketFilterMulticastAll;
+    }
+    else
+    {
+        result = super::getPacketFilters(group, filters);
+    }
+    return result;
+}
+
+IOReturn AirPort_Atheros9271::selectMedium(const IONetworkMedium *medium)
+{
+    setSelectedMedium(medium);
+    return kIOReturnSuccess;
+}
+
+IOReturn AirPort_Atheros9271::setMulticastMode(bool active)
+{
+    return kIOReturnSuccess;
+}
+
+IOReturn AirPort_Atheros9271::setMulticastList(IOEthernetAddress *addrs, UInt32 count)
+{
+    return kIOReturnSuccess;
+}
+
+IOReturn AirPort_Atheros9271::setPromiscuousMode(bool active)
+{
     return kIOReturnSuccess;
 }
 
@@ -128,8 +207,7 @@ void AirPort_Atheros9271::setDevice(IOService *provider)
     if (fDevice)
         fDevice->retain();
     
-    if (prevDevice)
-        prevDevice->release();
+    OSSafeReleaseNULL(prevDevice);
 }
 
 void AirPort_Atheros9271::printDeviceInfo()
@@ -151,13 +229,20 @@ void AirPort_Atheros9271::pushDeviceFirmware()
     if (setConfiguration(0) && findInterface() && fInterface->open(this))
     {
         IOLog("[%04x:%04x]: Configuration has been set and interface opened.\n", fVendorId, fProductId);
-        findPipe(fInterruptPipe, kEndpointTypeInterrupt, kEndpointDirectionIn);
-        findPipe(fBulkPipe, kEndpointTypeBulk, kEndpointDirectionOut);
+//        findPipe(fInterruptPipe, kEndpointTypeInterrupt, kEndpointDirectionIn);
+//        findPipe(fBulkPipe, kEndpointTypeBulk, kEndpointDirectionOut);
+//
+//        if (fInterruptPipe != NULL && fBulkPipe != NULL)
+//        {
+//            IOLog("[%04x:%04x]: Got the pipes.\n", fVendorId, fProductId);
+//        }
         
-        if (fInterruptPipe != NULL && fBulkPipe != NULL)
+        if (performUpgrade())
         {
-            IOLog("[%04x:%04x]: Got the pipes.\n", fVendorId, fProductId);
+            IOLog("[%04x:%04x]: Firmware upgrade completed successfully.\n", fVendorId, fProductId);
         }
+        else
+            IOLog("[%04x:%04x]: Firmware upgrade failed.\n", fVendorId, fProductId);
         
         fInterface->close(this);
     }
@@ -176,11 +261,6 @@ void AirPort_Atheros9271::pushDeviceFirmware()
     
     setInterface(NULL);
     fDevice->close(this);
-}
-
-void AirPort_Atheros9271::readCompletion(void *target, void *parameter, IOReturn status, UInt32 bufferSizeRemaining)
-{
-    
 }
 
 bool AirPort_Atheros9271::setConfiguration(int configurationIndex)
@@ -258,6 +338,33 @@ int AirPort_Atheros9271::getDeviceStatus()
     return (int)stat;
 }
 
+IOReturn AirPort_Atheros9271::pipeCommand(UInt8 requestType, UInt8 command, UInt16 address, IOMemoryDescriptor *buffer)
+{
+    DeviceRequest request;
+    request.bmRequestType = requestType;
+    request.bRequest = command;
+    request.wValue = address;
+    request.wIndex = 0;
+    request.wLength = buffer->getLength();
+    
+    uint32_t bytesTransferred;
+    return fInterface->deviceRequest(request, buffer, bytesTransferred, kUSBHostStandardRequestCompletionTimeout);
+}
+
+IOReturn AirPort_Atheros9271::pipeCommand(UInt8 requestType, UInt8 command, UInt16 address, void *buffer, UInt16 length)
+{
+    DeviceRequest request;
+    request.bmRequestType = requestType;
+    request.bRequest = command;
+    request.wValue = address;
+    request.wIndex = 0;
+    request.wLength = length;
+    
+    uint32_t bytesTransferred;
+    return fInterface->deviceRequest(request, buffer, bytesTransferred, kUSBHostStandardRequestCompletionTimeout);
+}
+
+
 IOReturn AirPort_Atheros9271::getDeviceConfiguration(UInt8 *configNumber)
 {
     uint8_t config = 0;
@@ -275,43 +382,61 @@ IOReturn AirPort_Atheros9271::getDeviceConfiguration(UInt8 *configNumber)
     return result;
 }
 
-IOReturn AirPort_Atheros9271::hciCommand(void *command, UInt16 length)
-{
-    DeviceRequest request;
-    request.bmRequestType = makeDeviceRequestbmRequestType(kRequestDirectionOut, kRequestTypeClass, kRequestRecipientDevice);
-    request.bRequest = 0;
-    request.wValue = 0;
-    request.wIndex = 0;
-    request.wLength = length;
-    
-    uint32_t bytesTransferred;
-    return fInterface->deviceRequest(request, command, bytesTransferred, 0);
-}
-
-IOReturn AirPort_Atheros9271::readFromPipe(IOUSBHostPipe *pipe, IOMemoryDescriptor *buffer, UInt32 length, IOUSBHostCompletion *completion, UInt32 *bytesRead)
+IOReturn AirPort_Atheros9271::readFromPipe(IOUSBHostPipe *pipe, IOMemoryDescriptor *buffer, IOByteCount length, IOUSBHostCompletion *completion, IOByteCount *bytesRead)
 {
     IOReturn result;
     if (completion)
-        result = pipe->io(buffer, length, completion);
+        result = pipe->io(buffer, (UInt32)length, completion);
     else
     {
         uint32_t bytesTransferred;
-        result = pipe->io(buffer, length, bytesTransferred);
+        result = pipe->io(buffer, (UInt32)length, bytesTransferred);
         if (bytesRead) *bytesRead = bytesTransferred;
     }
     
     return result;
 }
 
-IOReturn AirPort_Atheros9271::writeToPipe(IOUSBHostPipe *pipe, IOMemoryDescriptor *buffer, UInt32 length, IOUSBHostCompletion *completion)
+IOReturn AirPort_Atheros9271::writeToPipe(IOUSBHostPipe *pipe, IOMemoryDescriptor *buffer, IOByteCount length, IOUSBHostCompletion *completion)
 {
     IOReturn result;
     if (completion)
-        result = pipe->io(buffer, length, completion);
+        result = pipe->io(buffer, (UInt32)length, completion);
     else
     {
         uint32_t bytesTransferred;
-        result = pipe->io(buffer, length, bytesTransferred);
+        result = pipe->io(buffer, (UInt32)length, bytesTransferred);
+    }
+    
+    return result;
+}
+
+IOReturn AirPort_Atheros9271::bulkWrite(const void *data, UInt16 length)
+{
+    IOReturn result;
+    if (IOMemoryDescriptor *buffer = IOMemoryDescriptor::withAddress((void*)data, length, kIODirectionIn))
+    {
+        if ((result = buffer->prepare()) == kIOReturnSuccess)
+        {
+            if ((result = writeToPipe(fBulkPipe, buffer, buffer->getLength(), NULL)) == kIOReturnSuccess)
+            {
+                IOLog("[%04x:%04x]: Wrote %d bytes to bulk pipe.\n", fVendorId, fProductId, length);
+            }
+            else
+                IOLog("[%04x:%04x]: Failed to write to bulk pipe (0x%08x).\n", fVendorId, fProductId, result);
+        }
+        else
+            IOLog("[%04x:%04x]: Failed to prepare bulk write memory buffer (0x%08x).\n", fVendorId, fProductId, result);
+        
+        if ((result = buffer->complete()) != kIOReturnSuccess)
+            IOLog("[%04x:%04x]: Failed to complete bulk write memory buffer (0x%08x).\n", fVendorId, fProductId, result);
+        
+        OSSafeReleaseNULL(buffer);
+    }
+    else
+    {
+        IOLog("[%04x:%04x]: Unable to allocate bulk write buffer.\n", fVendorId, fProductId);
+        result = kIOReturnNoMemory;
     }
     
     return result;
@@ -339,7 +464,7 @@ bool AirPort_Atheros9271::findInterface()
         }
     }
     
-    iterator->release();
+    OSSafeReleaseNULL(iterator);
     
     IOLog("[%04x:%04x]: Interface %d (class %02x, subclass %02x, protocol %02x) located.\n", fVendorId, fProductId,
           fInterface->getInterfaceDescriptor()->bInterfaceNumber,
@@ -378,7 +503,7 @@ bool AirPort_Atheros9271::findPipe(IOUSBHostPipe *pipe, UInt8 type, UInt8 direct
             }
             
             setPipe(pipe, p);
-            pipe->release();
+            OSSafeReleaseNULL(pipe);
             return true;
         }
     }
@@ -389,17 +514,91 @@ bool AirPort_Atheros9271::findPipe(IOUSBHostPipe *pipe, UInt8 type, UInt8 direct
 
 bool AirPort_Atheros9271::performUpgrade()
 {
-    OSArray *instructions = NULL;
-    OSCollectionIterator *iterator = NULL;
-    OSData *data;
+    IOLog("[%04x:%04x]: Performing firmware upgrade.\n", fVendorId, fProductId);
     
+    OSData *firmwareData = OSData::withBytes(ar9271_fw, ar9271_fw_len);
+    bool success = true;
+    
+    IOLockLock(fCompletionLock);
+    
+    IOReturn result;
+    if (IOMemoryDescriptor *buffer = IOMemoryDescriptor::withAddress((void*)firmwareData->getBytesNoCopy(), firmwareData->getLength(), kIODirectionIn))
+    {
+        if ((result = buffer->prepare()) == kIOReturnSuccess)
+        {
+            if ((result = pipeCommand(0x40, FIRMWARE_DOWNLOAD, AR9271_FIRMWARE >> 8, buffer)) != kIOReturnSuccess)
+                IOLog("[%04x:%04x]: Unable to write the firmware (0x%08x).\n", fVendorId, fProductId, result);
+            else
+            {
+                if ((result = pipeCommand(0x40, FIRMWARE_DOWNLOAD_COMP, AR9271_FIRMWARE_TEXT >> 8, NULL, 0)) != kIOReturnSuccess)
+                    IOLog("[%04x:%04x]: Unable to write the firmware complete sequence (0x%08x).\n", fVendorId, fProductId, result);
+                else
+                {
+                    IOLog("[%04x:%04x]: Success in writing the firmware sequence.\n", fVendorId, fProductId);
+                    success = true;
+                }
+            }
+        }
+        else
+            IOLog("[%04x:%04x]: Failed to prepare write memory buffer (0x%08x).\n", fVendorId, fProductId, result);
+        
+        if ((result = buffer->complete()) != kIOReturnSuccess)
+            IOLog("[%04x:%04x]: Failed to complete write memory buffer (0x%08x).\n", fVendorId, fProductId, result);
+        
+        OSSafeReleaseNULL(buffer);
+    }
+    else
+        IOLog("[%04x:%04x]: Unable to allocate write memory buffer.\n", fVendorId, fProductId);
+    
+    IOLockUnlock(fCompletionLock);
+    OSSafeReleaseNULL(firmwareData);
+    return success;
+}
+
+bool AirPort_Atheros9271::createMediumTables()
+{
+    IOLog("Creating medium tables for AirPortAtheros9271 driver.\n");
+    IONetworkMedium *medium;
+    
+    OSDictionary *mediumDict = OSDictionary::withCapacity(1);
+    if (mediumDict == NULL)
+    {
+        IOLog("Cannot allocate medium dictionary.\n");
+        return false;
+    }
+    
+    medium = IONetworkMedium::medium(kIOMediumEthernetAuto, 480 * 1000000);
+    IONetworkMedium::addMedium(mediumDict, medium);
+    OSSafeReleaseNULL(medium);
+    
+    bool setResult = false;
+    bool result = publishMediumDictionary(mediumDict);
+    if (!result)
+        IOLog("Cannot publish medium dictionary.\n");
+    else
+    {
+        medium = IONetworkMedium::getMediumWithType(mediumDict, kIOMediumEthernetAuto);
+        setResult = setCurrentMedium(medium);
+    }
+    
+    OSSafeReleaseNULL(mediumDict);
+    return result && setResult;
+}
+
+bool AirPort_Atheros9271::createNetworkInterface()
+{
+    IOLog("Attaching and registering for AirPortAtheros9271 driver.\n");
+ 
+    if (!attachInterface((IONetworkInterface**)&fNetworkInterface, true))
+    {
+        IOLog("Attach interface failed.\n");
+        return false;
+    }
+    
+    IOLog("Network Interface %p", fNetworkInterface);
     return true;
 }
 
-bool AirPort_Atheros9271::continuousRead()
-{
-    return true;
-}
 
 void AirPort_Atheros9271::setInterface(IOService *interface)
 {
@@ -409,19 +608,17 @@ void AirPort_Atheros9271::setInterface(IOService *interface)
     if (fInterface)
         fInterface->retain();
     
-    if (prev)
-        prev->release();
+    OSSafeReleaseNULL(prev);
 }
 
 void AirPort_Atheros9271::setPipe(IOUSBHostPipe *pipe, OSObject *p)
 {
     OSObject *prev = pipe;
     pipe = OSDynamicCast(IOUSBHostPipe, p);
-    
+
     if (pipe)
         pipe->retain();
-    
-    if (prev)
-        prev->release();
+
+    OSSafeReleaseNULL(prev);
 }
 
--- AirPortAtheros9271/AirPortAtheros9271.hpp
+++ AirPortAtheros9271/AirPortAtheros9271.hpp
@@ -5,56 +5,58 @@
 #pragma clang diagnostic ignored "-Weverything"
 #pragma clang diagnostic ignored "-Wdocumentation"
 #include <IOKit/network/IOEthernetController.h>
+#include <IOKit/network/IOEthernetInterface.h>
+#include <IOKit/network/IOOutputQueue.h>
 #include <IOKit/IOService.h>
 #pragma clang diagnostic pop
 
-enum DeviceState
-{
-    kAthUnknown,
-    kAthInitialize,
-    kAthFirmwareWrite,
-    kAthFirmwareWritten,
-    kAthResetComplete,
-    kAthUpdateComplete,
-    kAthUpdateNotNeeded,
-    kAthUpdateAborted,
-};
+#define AR9271_FIRMWARE 0x501000
+#define AR9271_FIRMWARE_TEXT 0x903000
+
+#define FIRMWARE_DOWNLOAD 0x30
+#define FIRMWARE_DOWNLOAD_COMP 0x31
 
 class AirPort_Atheros9271 : public IOEthernetController
 {
     OSDeclareDefaultStructors(AirPort_Atheros9271);
     
 protected:
-    typedef IOService super;
+    typedef IOEthernetController super;
     
     IOUSBHostDevice *fDevice;
     IOUSBHostInterface *fInterface;
     
     IOUSBHostPipe *fBulkPipe;
-    IOUSBHostPipe *fControlPipe;
     IOUSBHostPipe *fInterruptPipe;
+    
+    IOEthernetInterface *fNetworkInterface;
+    IONetworkStats *fNetworkStats;
+    
+    IOLock *fCompletionLock = NULL;
     
     bool setConfiguration(int configurationIndex);
     bool resetDevice();
     bool findInterface();
     bool findPipe(IOUSBHostPipe *pipe, UInt8 type, UInt8 direction);
     bool performUpgrade();
-    bool continuousRead();
+    bool createMediumTables();
+    bool createNetworkInterface();
+
     
     void setDevice(IOService *provider);
     void setInterface(IOService *interface);
     void setPipe(IOUSBHostPipe *pipe, OSObject *p);
     void printDeviceInfo();
     void pushDeviceFirmware();
-    void readCompletion(void *target, void *parameter, IOReturn status, UInt32 bufferSizeRemaining);
     
     int getDeviceStatus();
     
+    IOReturn pipeCommand(UInt8 requestType, UInt8 command, UInt16 address, IOMemoryDescriptor *buffer);
+    IOReturn pipeCommand(UInt8 requestType, UInt8 command, UInt16 address, void *buffer, UInt16 length);
     IOReturn getDeviceConfiguration(UInt8 *configNumber);
-    IOReturn hciCommand(void *command, UInt16 length);
-    IOReturn readFromPipe(IOUSBHostPipe *pipe, IOMemoryDescriptor *buffer, UInt32 length, IOUSBHostCompletion *completion, UInt32 *bytesRead);
-    IOReturn writeToPipe(IOUSBHostPipe *pipe, IOMemoryDescriptor *buffer, UInt32 length, IOUSBHostCompletion *completion);
-    IOReturn bulkWrite(const void *data, uint16_t length);
+    IOReturn readFromPipe(IOUSBHostPipe *pipe, IOMemoryDescriptor *buffer, IOByteCount length, IOUSBHostCompletion *completion, IOByteCount *bytesRead);
+    IOReturn writeToPipe(IOUSBHostPipe *pipe, IOMemoryDescriptor *buffer, IOByteCount length, IOUSBHostCompletion *completion);
+    IOReturn bulkWrite(const void *data, UInt16 length);
     
 private:
     UInt16 fVendorId;
@@ -74,14 +76,26 @@ public:
     virtual bool start(IOService *provider) override;
     virtual void stop(IOService *provider) override;
     
-    virtual IOReturn message(UInt32 type, IOService *provider, void *argument) override;
-
     virtual bool willTerminate(IOService *provider, IOOptionBits options) override;
     virtual bool terminate(IOOptionBits options = 0) override;
     virtual bool finalize(IOOptionBits options) override;
     
+    virtual IOReturn message(UInt32 type, IOService *provider, void *argument) override;
+    
     // IOEtherenetController overrides
     virtual IOReturn getHardwareAddress(IOEthernetAddress *addr) override;
+    virtual IOReturn getMaxPacketSize(UInt32 *maxSize) const override;
+    virtual IOReturn getPacketFilters(const OSSymbol *group, UInt32 *filters) const override;
+    
+    virtual IONetworkInterface *createInterface() override;
+    virtual bool configureInterface(IONetworkInterface *iface) override;
+    
+    virtual IOReturn enable(IONetworkInterface *iface) override;
+    virtual IOReturn disable(IONetworkInterface *iface) override;
+    virtual IOReturn selectMedium(const IONetworkMedium *medium) override;
+    virtual IOReturn setMulticastMode(bool active) override;
+    virtual IOReturn setMulticastList(IOEthernetAddress *addrs, UInt32 count) override;
+    virtual IOReturn setPromiscuousMode(bool active) override;
 };
 
 #endif
